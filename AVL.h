a2929dc6428776deb336537a313216191e385c8e
Dmkn78 <dmkn78@gmail.com>
2024-12-10 15:05:40 +0100

    Update AVL.h

--- AVL.h
+++ AVL.h
@@ -2,15 +2,15 @@
 #define AVL.H
 
 // Structure pour représenter un nœud de l'arbre AVL
-typedef struct node{
-  int id;
-  int id_centrale;
-  long long capacite;
-  long long consommation;
-  int hauteur;
-  struct node * droite;
-  struct node *gauche;
-}NoeudAVL;
+typedef struct NoeudAVL {
+    int id;                     // Identifiant de la station
+    int id_centrale;            // Identifiant de la centrale
+    long long capacite;         // Capacité de la station
+    long long consommation;     // Consommation de la station
+    int hauteur;                // Hauteur du nœud
+    struct NoeudAVL *gauche;    // Sous-arbre gauche
+    struct NoeudAVL *droite;    // Sous-arbre droit
+} NoeudAVL;
 
 
 
