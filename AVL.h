54264cc37d22a81ea237ca5c9516c552ac7a2bd3
Dmkn78 <dmkn78@gmail.com>
2024-12-10 17:15:56 +0100

    Update AVL.h

--- AVL.h
+++ AVL.h
@@ -1,5 +1,8 @@
 #ifndef AVL.H
 #define AVL.H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // Structure pour représenter un nœud de l'arbre AVL
 typedef struct NoeudAVL {
@@ -12,11 +15,24 @@ typedef struct NoeudAVL {
     struct NoeudAVL *droite;    // Sous-arbre droit
 } NoeudAVL;
 
+int max(int a, int b);
+
+NoeudAVL* creerNoeudAVL(char* elts[]);
+
+NoeudAVL* rotationGauche(NoeudAVL* n);
+NoeudAVL* rotationDroite(NoeudAVL* n);
+NoeudAVL* rotationGaucheDroite(NoeudAVL* n);
+NoeudAVL* rotationDroiteGauche(NoeudAVL* n);
+
+NoeudAVL* equilibrer(NoeudAVL* n);
+NoeudAVL* insererAVL(NoeudAVL* racine, NoeudAVL* nouveau);
+
+NoeudAVL* rechercher(NoeudAVL* racine, int id);
+
+void traiter_Arbre(NoeudAVL* racine);
+void parcoursPrefixe(NoeudAVL* racine);
+
+void free_Arbre(NoeudAVL* racine);
 
 
-
-NoeudAVL *creerNoeud(char *elt[]);
-NoeudAVL* inserer(NoeudAVL* n, NoeudAVL* nouveau);
-NoeudAVL *rechercher(NoeudAVL *n, int id);
-void parcours_pre(NoeudAVL * n);
-
+#endif
