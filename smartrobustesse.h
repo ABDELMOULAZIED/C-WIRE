9702f835a7da3a96053c1d473483a0d41bdfed12
Dmkn78 <dmkn78@gmail.com>
2024-12-12 11:34:46 +0100

    Create smartrobustesse.h

---
+++ smartrobustesse.h
@@ -0,0 +1,15 @@
+#ifndef SMARTROBUSTESSE_H
+#define SMARTROBUSTESSE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "avl.h"
+
+// Taille maximale des lignes et éléments
+#define TAILLE_MAX 256
+
+// Fonction de vérification de l'allocation mémoire 
+void* safeMalloc(size_t size);
+
+/
+#endif // SMARTROBUSTESSE_H
