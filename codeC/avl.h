aa0c7524d5a2b115c500ebd16e350d59e9c06a50
danbvm958 <dan.bavamian958@gmail.com>
2024-12-15 23:04:07 +0100

    Update and rename avl.c to avl.h

---
+++ codeC/avl.h
@@ -0,0 +1,28 @@
+#ifndef AVL_H
+#define AVL_H
+
+typedef struct node {
+    int id;
+    int id_centrale;
+    long long capacite;
+    long long consommation;
+    int hauteur;
+    struct node *droite;
+    struct node *gauche;
+} NoeudAVL;
+
+int max(int a, int b);
+NoeudAVL *creerNoeudAVL(char *elts[]);
+NoeudAVL *rotationGauche(NoeudAVL *racine);
+NoeudAVL *rotationDroite(NoeudAVL *racine);
+NoeudAVL *insererAVL(NoeudAVL *racine, NoeudAVL *nouveau);
+NoeudAVL *rechercher(NoeudAVL *n, int id);
+NoeudAVL *MEP_Stations(FILE *fic);
+void MAJ_Stations(FILE *fic, NoeudAVL *n);
+void parcoursPrefixe(NoeudAVL *n);
+NoeudAVL* equilibrer(NoeudAVL* n);
+
+void traiter_Arbre(NoeudAVL* racine);
+void liberer_Arbre(NoeudAVL* racine);
+
+#endif // AVL_H
